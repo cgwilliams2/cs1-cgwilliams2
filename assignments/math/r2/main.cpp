401ff89a3720657a8503662aaf14f428c9b7b4a5
Colton Williams <cgwilliams2@mavs.coloradomesa.edu>
2023-02-21 14:16:33 -0700

    finished code and added screenshot of kattis checking code, stil need to add local test cases

---
+++ assignments/math/r2/main.cpp
@@ -0,0 +1,19 @@
+//Colton Williams R2 kattis problem 02/21/2023
+
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+    int r1, r2, s;
+    
+    cin >> r1 >> s; 
+    
+    r2 = 2 * s - r1; 
+    
+    cout << r2; 
+
+
+    return 0;
+}
