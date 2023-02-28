7d7086ba471ee392e5394231e99c16c4ae8bd94b
Colton Williams <cgwilliams2@mavs.coloradomesa.edu>
2023-02-27 21:10:18 -0700

    updated hw3

---
+++ assignments/hw3/calculator/main.cpp
@@ -0,0 +1,45 @@
+// Colton Williams HW3 Calculator 
+//02/27/2023
+
+#include <iostream> 
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main (){
+
+   double add (double one, double two) {
+
+        return one + two; 
+
+
+    }
+
+   
+    double main1, main2;
+    cout << "Enter a number \n" << endl; 
+    cin >> main1; 
+    cout << "Enter another number \n" << endl; 
+    cin >> main2;
+
+    cout << main1 << " + " << main2 << " = " << add (main1 , main2) << endl; 
+
+
+
+
+
+ 
+
+
+    
+
+
+
+
+
+
+
+
+    return 0; 
+}
