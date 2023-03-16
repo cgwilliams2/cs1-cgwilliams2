aa55836765e8bc7eb26f0844ac91333b4ac0f79c
Colton Williams <cgwilliams2@mavs.coloradomesa.edu>
2023-03-16 15:15:17 -0600

    working on test

--- assignments/loops/fizzbuzz/main.cpp
+++ assignments/loops/fizzbuzz/main.cpp
@@ -9,16 +9,19 @@ Print integers from
 
 #include <iostream>
 #include <cassert>
+#include <string>
 
 using namespace std; 
 
 void out(int x, int y, int n);
 void test();
+string check = "";
 
 int main(){
     int x, y, n; 
     cin >> x >> y >> n; 
     out(x, y, n);
+    
 
     return 0; 
 }
@@ -26,22 +29,43 @@ int main(){
 
 void out(int x, int y, int n){
     for(int i = 1; i <= n; i++){
-        if(i % x == 0 && i % y != 0 )
-            cout << "Fizz" << endl; 
-        if(i % y == 0 && i % x != 0)
-            cout << "Buzz" << endl; 
-        if(i % x == 0 && i % y == 0)
+
+        if(i % x == 0 && i % y != 0 ){
+            cout << "Fizz" << endl;
+            check += "1";
+            }
+             
+        if(i % y == 0 && i % x != 0){
+            cout << "Buzz" << endl;
+            check += "2";
+            }
+             
+        if(i % x == 0 && i % y == 0){
             cout << "FizzBuzz" << endl;
-        if(i % x != 0 && i % y != 0)
-            cout << i << endl; 
+            check += "3";
+            }
+            
+        if(i % x != 0 && i % y != 0){
+            cout << i << endl;
+            check +="4";
+            }
+             
         
     }
 
 }
 
 void test(){
-
     int x, y, n;
+    string ans; 
+    x = 2;
+    y = 3;
+    n = 7;
+    ans = "4121434";
+    out(x, y, n);
+    assert(ans-check;)
+
+    
 
 
     cerr << "Local test cases passed!\n";
