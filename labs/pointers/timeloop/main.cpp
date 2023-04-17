102eace3e0029f35a7e78cb3355b2cc636519087
Colton Williams <cgwilliams2@mavs.coloradomesa.edu>
2023-04-17 14:50:25 -0600

    codeing timeloop

--- labs/pointers/timeloop/main.cpp
+++ labs/pointers/timeloop/main.cpp
@@ -1,216 +1,80 @@
-//  Pointers, enum type and namespace lab
-//  Updated by: Colton Williams
-//  Date: 04/17/2023
-//  Program demonstrates the use of preprocessor directive, namespace, enum type,
-//  and pointers.
+/*
+Kattis - Stuck In A Time Loop
+Loop Lab
+Updated By: FIXME
+CSCI 111
+Date: FIXME
 
-#include <cstdio>
+Read and solve the Kattis problem: https://open.kattis.com/problems/timeloop  
+
+Algorithm Steps:
+  1. Using a function, read and return N
+  2. Declare a dynamic variable to store, N returned by function in 1.
+  3. Define a function to get the necessary spell given N
+  4. Define a function to print answer
+    4.i Within the function, use a loop to call spell function to print spell N times:
+      1 Abracadabra
+      2 Abracadabra
+      ...
+      N Abracadabra
+*/
+
 #include <iostream>
 #include <cassert>
 #include <string>
-
+#include <iomanip>
+#include <cmath>
+#include <sstream>
 using namespace std;
 
-// macro function
-#ifdef _WIN32
-#define clear() system("cls")
-#else
-#define clear() system("clear")
-#endif
+// declare alias pos_short of type "unsigned int"
+using pos_short = unsigned short;
 
-// user-defined enumerated type: OPERATION
-enum OPERATION { ADD, MULTIPLY, SUBTRACT, LARGER, QUIT };
+// Function taken an integer n and returns
+// spell: n Abracadabra
+string spell(const pos_short);
 
-// C++ way of defining type alias
-using big_int = long long int;
+// function to test spell function
+void testSpell();
 
-// C way of defining type alias
-typedef long long int large_int;
-// both large_int and big_int are alias to "long long int" type
+// function to print the final answer
+void printAnswer(const pos_short);
 
-// declare MyFunctions namespace
-namespace MyFunctions {
-    // function prototypes defined within the namespace
-    big_int findSum(const big_int *, const big_int *);
-    big_int findLarger(const big_int *, const big_int *);
-    large_int findProduct(const large_int &, const large_int &);
-    large_int findDifference(const large_int &, const large_int &);
-}
+// function to read N
+pos_short readNumber();
 
-void showMenu(void);
-OPERATION getOperation(char);
-void unittest();
-
-  int main(int argc, char* argv[]) {
-    if (argc == 2 and string(argv[1]) == "test"){
-        unittest();
-        return 0; 
-        }
-    
-
-    // FIXME1 - Call unittest function if "test" argument is passed to main FIXED
-    // exit the program after calling unittest
-
-    char input;
-    big_int *num1; //a pointer variable to point to big_int data
-    num1 = new big_int; //allocate memory in heap and store the address in num1
-    big_int *num2 = new big_int; //declare num2 as a dynamic variable
-    large_int *sum = new large_int; //declare sum as a dynamic variable
-    large_int *prod = new large_int;
-    large_int *diff = new large_int;
-    big_int *larger = new big_int; 
-    // FIXME2 - declare dynamic variables to store difference and larger values
-    // Must use these variables to store the returned values from functions
-
-    OPERATION oper;
-    
-    //FIXME3: Add do... while loop to continue the program until the user wants to quit
-    //FIXME4: call clear function defined above to clear the screen
-    clear();
-    do {
-    showMenu();
-    cin >> input;
-    oper = getOperation(input);
-    switch (oper) {
-        case ADD:
-            cout << "Enter two whole numbers separated by space: ";
-            // store the data by dereferencing pointers
-            cin >> *num1 >> *num2;
-            // passing pointers to findSum function
-            *sum = MyFunctions::findSum(num1, num2);
-            printf("%lld + %lld = %lld\n", *num1, *num2, *sum);
-            break;
-            // FIXME5: complete the rest of the cases to perform other operations
-        case MULTIPLY:
-            cout << "Enter two whole numbers separated by space: ";
-            // store the data by dereferencing pointers
-            cin >> *num1 >> *num2;
-            // passing dereferences (actural addresses) of num1 and num2
-            *prod = MyFunctions::findProduct(*num1, *num2);
-            printf("%lld * %lld = %lld\n", *num1, *num2, *prod);
-            break;
-        case SUBTRACT:
-            cout << "Enter two whole numbers separated by space: ";
-            // store the data by dereferencing pointers
-            cin >> *num1 >> *num2;
-            // passing dereferences (actural addresses) of num1 and num2
-            *diff = MyFunctions::findDifference(*num1, *num2);
-            printf("%lld - %lld = %lld\n", *num2, *num1, *diff);
-            break;
-        case LARGER:
-            cout << "Enter two whole numbers separated by space: ";
-            // store the data by dereferencing pointers
-            cin >> *num1 >> *num2;
-            // passing dereferences (actural addresses) of num1 and num2
-            *larger = MyFunctions::findLarger(num1, num2);
-            printf("%lld, %lld |  %lld is bigger\n", *num1, *num2, *larger);
-            break; 
-        default:
-            break;
-    }
-    }while (oper != QUIT);
-    // delete/deallocate/free memory occupied by all the dynamic variables
-    delete num1;
-    delete num2;
-    delete sum;
-    delete prod;
-    delete diff;
-    delete larger;
-
-    cin.ignore(1000, '\n');
-    cout << "Good bye! Enter to exit the program...";
-    cin.get();
+int main(int argc, char* argv[]) {
+  if (argc == 2 and string(argv[1]) == string("test")) {
+    // FIXME1: call testSpell function
     return 0;
+  }
+  // declare a dynamic variable to store N
+  pos_short *N = new pos_short; 
+  //FIXME2: Call readNumber function and assign the returned value to N;
+  //FIXME3: Call answer function passing proper argument
 }
 
-// Menu options
-void showMenu(void) {
-    cout << "Enter one of the following menu options:\n";
-    cout << "[1] Add two integers\n";
-    cout << "[2] Multiply two integers\n";
-    cout << "[3] Subtract one integer from another\n";
-    cout << "[4] Find the larger of two integers\n",
-    cout << "[5] Quit the program\n";
-    cout << "Enter your choice [1-4]: ";
+pos_short readNumber() {
+  //FIXME4: Declare a dynamic variable to read and store N and return it
+  pos_short *N;
+  return *N;
 }
 
-// function returns OPERATION type given character choice
-OPERATION getOperation(char choice) {
-    OPERATION op;
-    switch(choice)
-    {
-        case '1':
-            op = ADD;
-            break;
-        case '2':
-            op = MULTIPLY;
-            break;
-        case '3':
-            op = SUBTRACT;
-            break;
-        case '4':
-            op = LARGER;
-            break;     // FIXME6 - add case for LARGER FIXED
-        default:
-            op = QUIT;
-        
-    }
-    return op;
+string spell(pos_short n) {
+  return to_string(n) + " Abracadabra";
 }
 
-// must use MyFunctions namespace to resolve findSum function
-big_int MyFunctions::findSum(const big_int *n1, const big_int *n2) {
-    // dereference pointers n1 and n2 before adding their values
-    return (*n1) + (*n2);
+void testSpell() {
+  pos_short * n = new pos_short;
+  *n = 10;
+  assert(spell(*n) == "10 Abracadabra");
+  // FIXME5: Add two more test cases
+  delete n;
+  cerr << "Yay... all test cases passed!\n";
 }
 
-// FIXME7: define findLarger function declared inside MyFunctions namespace FIXED 
-// function returns the larger of the two given numbers
-
-big_int MyFunctions::findLarger(const big_int *n1, const big_int *n2) {
-
-    if((*n1) > (*n2))
-        return (*n1); 
-    else return (*n2); 
-} 
-
-
-// find Product function definition
-large_int MyFunctions::findProduct(const large_int &n1, const large_int &n2) {
-    return n1 * n2;
-}
-
-// FIXME8: define findDifference function declared inside MyFunctions namespace
-// return the value of second big_int subtracted from the first
-large_int  MyFunctions::findDifference(const large_int &n1, const large_int &n2) {
-
-    return n2 - n1; 
-}
-
-// function to unittest other functions
-void unittest() {
-    big_int *n1, *n2, *ans;
-    n1 = new big_int;
-    *n1 = 100000000;
-    n2 = new big_int;
-    *n2 = 999999999;
-    ans = new big_int;
-    *ans = MyFunctions::findSum(n1, n2);
-    assert(*ans == 1099999999);
-    *n1 = 93;
-    *n2 = 837;
-    *ans = MyFunctions::findLarger(n1, n2);
-    assert(*ans == 837);
-    *n1 = 50;
-    *n2 = 25;
-    *ans = MyFunctions::findDifference(*n1, *n2);
-    assert(*ans == -25);
-    *n1 = 5;
-    *n2 = 20;
-    *ans = MyFunctions::findProduct(*n1, *n2);
-    assert(*ans == 100);
-    
-    cerr << "Yay... All test cases passed!\n";
-    // FIXME9: Test findProduct, findDifference and findLarger functions with atleast 1 test case
-    // FIXME10: Delete all the dynamic variables
+void printAnswer(const pos_short N) {
+  for(pos_short i=1; i<=N; i++) {
+    printf("%s\n", spell(i).c_str());
+  }
 }
